2d5c37ef9eb88e84d2b57a6bc870ba8ceaf99925
rosh <roshbon@gmail.com>
2022-10-10 00:54:41 +0100

    added contact me

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const Joi = require('joi')
 const port = 6500
+const mysql = require('mysql2')
+const { v4: uuidv4 } = require('uuid');
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+})
 
 app.use(bodyParser.json())
 
@@ -12,13 +22,12 @@ app.listen(port, () => {
 })
 
 
-app.post('/customer', (req, res) => {
+app.post('/message', (req, res) => {
 
     const schema = Joi.object({
-        firstname: Joi.string().min(3).required(),
-        lastname: Joi.string().min(3).required(),
-        phone: Joi.string().min(11).max(15).required(),
-        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        fullname: Joi.string().min(3).required(),
+        email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+        message: Joi.string().min(2).required()
     })
 
     const { error, value } = schema.validate(req.body)
@@ -31,51 +40,28 @@ app.post('/customer', (req, res) => {
     }
 
 
-    const firstname = req.body.firstname
-    const lastname = req.body.lastname
-    const phone_number = req.body.phone
-    const email = req.body.email
+    // const fullname = req.body.fullname
+    // const email = req.body.email
+    // const message = req.body.message
 
-    // if (firstname == "" || lastname == "" || phone_number == "" || email == "") {
-    //     res.status(400).send({
-    //         status: false,
-    //         message: "All fields are mandatory"
-    //     })
-    // }
+    const { fullname,email, message } = req.body
+    const user_id = uuidv4()
+    connection.query(
+        `INSERT into details(_id,fullname,email,message) values('${user_id}','${fullname}','${email}','${message}')`,
+        (err, results, fields) => {
+            if (err) {
+                console.log("err: " , err)
+                res.status(400).send({
+                    status: false,
+                    message: "Try again please"
+                }) 
+            }
+        }
+    )
 
-    //do some database insert of the data
-    res.send({
+    res.status(201).send({
         status: true,
-        message: "customer successfully created"
+        message: "Message successfully sent"
     })
 })
 
-app.get('/customer', (req, res) => {
-    
-
-    res.status(200).send({
-        status: true,
-        message: "All data fetched"
-    })
-
-})
-
-app.patch('/customer', (req, res) => {
-    
-
-    res.status(200).send({
-        status: true,
-        message: "Data successfully updated"
-    })
-
-})
-
-app.delete('/customer', (req, res) => {
-    
-
-    res.status(200).send({
-        status: true,
-        message: "Data successfully deleted"
-    })
-
-})
